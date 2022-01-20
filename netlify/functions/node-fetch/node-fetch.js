f2266cccdfbc8639a474c7f6ad952833a136d35b
Kishuan Espiritu <kespiritu@myseneca.ca>
2022-01-19 19:14:30 -0500

    updated toml

--- netlify/functions/node-fetch/node-fetch.js
+++ netlify/functions/node-fetch/node-fetch.js
@@ -26,4 +26,23 @@ const handler = async function () {
   }
 }
 
+// const fetch = require("node-fetch");
+
+// const API_ENDPOINT = 'https://api.covid19tracker.ca/summary';
+
+// const handler = async (event, context) => {
+//   try {
+//     const response = await fetch(API_ENDPOINT);
+//     const data = await response.json();
+//     return { statusCode: 200, body: JSON.stringify({ data }) };
+//   } catch (error) {
+//     console.log(error);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Failed fetching data' }),
+//     };
+//   }
+// };
+          
+
 module.exports = { handler }
